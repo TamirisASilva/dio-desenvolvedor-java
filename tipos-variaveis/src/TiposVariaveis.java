d29c2327b658465cf254b11f9cbe66080955ceb3
Tamiris A Silva <desenv.tasilva@outlook.com>
2024-03-25 11:32:17 -0300

    Tipos de variaveis em java

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        byte idade = 30;
+        short ano = 2024;
+        int cep = 21051370;
+        long cpf = 16013786209L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        System.out.println(cep);
+
+    }
+}
