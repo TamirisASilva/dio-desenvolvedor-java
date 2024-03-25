82242dc2b1e6630284ca85434d410fbe3426716d
Tamiris A Silva <desenv.tasilva@outlook.com>
2024-03-25 10:50:49 -0300

    Começando

---
+++ anatomia-de-classes/src/edu/tamiris/primeirasemana/MinhaClasse.java
@@ -0,0 +1,15 @@
+package edu.tamiris.primeirasemana;
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Tamiris";
+        String segundoNome = "Alves";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do Metódo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
