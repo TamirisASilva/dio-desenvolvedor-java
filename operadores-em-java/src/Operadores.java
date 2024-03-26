8c10d900e04b9f02b7c74dbce5bf8a16766460cb
Tamiris A Silva <desenv.tasilva@outlook.com>
2024-03-26 09:55:25 -0300

    Aula sobre operadores, metódos e criação do projeto smart tv.

---
+++ operadores-em-java/src/Operadores.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nome = "Tamiris";
+        int idade = 30;
+        double peso = 70.5;
+        char sexo = 'F';
+        boolean doador = true;
+        Date dataNascimento = new Date();
+
+        int soma = 10 + 5;
+        double subtracao = 5.6 - 2.3;
+        int multiplicacao = 5 * 3;
+        int divisao = 9 / 3;
+        int resto = 9 % 4;
+        double resultado = (10 * 7) + (20/4);
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.print(dataNascimento);
+    }
+}
---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,39 @@
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void subirCanal() {
+        canal++;
+        System.out.println("Canal alterado para: " + canal);
+    }
+
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Canal alterado para: " + canal);
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+}
