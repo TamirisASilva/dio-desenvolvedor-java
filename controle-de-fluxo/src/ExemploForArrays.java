33ccad5c88a222e216ca4a89777320c7689402b4
Tamiris A Silva <desenv.tasilva@outlook.com>
2024-04-02 10:16:47 -0300

    Controles de fluxo em java

---
+++ controle-de-fluxo/src/ExemploForArrays.java
@@ -0,0 +1,10 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        // for (int x = 0; x < alunos.length; x++) {}
+        for (String aluno : alunos) {
+            System.out.println("O nome do aluno é: " + aluno);
+        }
+    }
+}
